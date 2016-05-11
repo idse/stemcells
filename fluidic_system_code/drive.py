d593554a42978020b6c7ee42a07b72f68c42e7ec
claytonlittle <clayton.little@live.com>
2016-05-11 18:01:32 -0500

    Added fluid control files

---
+++ fluidic_system_code/drive.py
@@ -0,0 +1,53 @@
+"""
+Contains functions which perform base-level communication with arduino.
+Functions:
+-setup                set up connection
+-m1                   drive first motor
+-m2                   drive second motor
+
+Name: drive
+Author: Clayton Little
+"""
+
+ser = 0  # initialize serial object
+
+def setup(port,br):
+    """
+    Sets up connection to the Arduino
+    port is int of USB port. Look at arduino console to find it.
+    br is the baud rate. This should be 9600.
+    """
+    import serial        # the PySerial package
+    global ser           # so other functions can access
+    ser = serial.Serial('COM'+str(port),br) # open serial connection
+    connected = False    # set up Boolean indicating connection status
+    ## Because the serial connection is not immediate, we want to
+    ## wait until it is available before sending commands:
+    while not connected:
+        serin = ser.read()
+        connected = True
+    print("READY")
+    print("Serial information:")
+    return ser
+    
+    
+def m1(dr,vol,spd=50):
+    """
+    Sends a single command to the first motor.
+    
+    dr is an int specifying direction. 1 is inject. 0 is remove.
+    vol is the volume transfer in microLiters.
+    spd is the speed of fluid transfer in microLiters per seconds.
+    """
+    ser.write(bytes(','.join(['1',str(dr),str(vol),str(spd)])+'*',"ascii"))
+    
+
+def m2(dr,vol,spd=50):
+    """
+    Sends a single command to the first motor.
+    
+    dr is an int specifying direction. 1 is inject. 0 is remove.
+    vol is the volume transfer in microLiters.
+    spd is the speed of fluid transfer in microLiters per seconds.
+    """
+    ser.write(bytes(','.join(['2',str(dr),str(vol),str(spd)])+'*',"ascii"))
---
+++ fluidic_system_code/fluidmaster.py
@@ -0,0 +1,13 @@
+"""
+Contains the fluid transfer commands.
+
+Name: fluidmaster
+Author: Clayton Little
+"""
+
+## Setup
+import drive
+drive.setup(3,9600)
+
+## Commands
+drive.m1(1,1000,1000)
